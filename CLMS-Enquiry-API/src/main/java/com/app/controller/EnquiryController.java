d6e28940f7ab9aab2dbc47c36ed56a33adc56d9f
Suraj <ingolesuraj53@gmail.com>
2025-03-21 16:49:36 +0530

    post method created

--- CLMS-Enquiry-API/src/main/java/com/app/controller/EnquiryController.java
+++ CLMS-Enquiry-API/src/main/java/com/app/controller/EnquiryController.java
@@ -1,10 +1,33 @@
 package com.app.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.app.entity.LoanEnquiry;
+import com.app.service.EnquiryService;
 
 @RestController
 @RequestMapping("/enquiry")
 public class EnquiryController {
+	
+	@Autowired
+	EnquiryService enquiryService;
+	
+	@PostMapping("/enquiry")
+	public ResponseEntity<String> saveEnquiry(@RequestBody LoanEnquiry enquiry){
+		
+		String msg = enquiryService.saveEnquiry(enquiry);
+		System.out.println(enquiry);
+		
+		return new  ResponseEntity<String>(msg , HttpStatus.OK);
+		
+		
+		
+	}
 
 }
